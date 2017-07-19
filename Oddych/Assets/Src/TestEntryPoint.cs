821d922cfec1e02e9950f8c509e6dfe0f674e296
ondro <ondrej.mikulas@gmail.com>
2017-07-19 15:25:25 +0200

    Finished not tested version of Restifizer which is using our Special libraries enabling nested coroutines call.

--- Oddych/Assets/Src/TestEntryPoint.cs
+++ Oddych/Assets/Src/TestEntryPoint.cs
@@ -23,19 +23,22 @@ namespace Oddych{
 			//Test3 test3 = gameObject.AddComponent<Test3> ();
 			//test3.Start ();
 
-			const String _URL = "http://jsonplaceholder.typicode.com/comments";
-			const String _WEBDATA = 
+			const String _URL = "http://jsonplaceholder.typicode.com/comments?postId=1";
+			//const String _WEBDATA = 
 				//"{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"some fancy title by Ondrej Mikulas\"}";
-				"{\n  \"userId\": 1,\n \"title\": \"some fancy title by Ondrej Mikulas\"}";
-
-			for (int i = 0; i < 10; i++) {
-				Debug.Log (DateTime.Now.ToString("hh:mm:ss.fff") +  " BEFORE getting data");
+				//"{\n  \"userId\": 1,\n \"title\": \"some fancy title by Ondrej Mikulas\"}";
+			const int _REPETITIONS = 1;
+			for (int i = 0; i < _REPETITIONS; i++) {
+				DateTime startTime = DateTime.Now;
 				RestMethods restMethods = gameObject.AddComponent<RestMethods> ();
-				//String data = restMethods.Get (_URL);
-				String data = restMethods.Post (_URL, _WEBDATA);
+
+				String data = restMethods.Get (_URL);
+				//String data = restMethods.Post (_URL, _WEBDATA);
 				//long data = restMethods.Delete (_URL);
+
 				Debug.Log (data);
-				Debug.Log(DateTime.Now.ToString("hh:mm:ss.fff") + " AFTER getting data");
+				DateTime endTime = DateTime.Now;
+				Debug.Log("delta (end - start): " + endTime.Subtract(startTime));
 			}
 		}
 
