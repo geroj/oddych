fc2d7b78d7b0d4d35ed9d702b004f37a93c2b876
ondro <ondrej.mikulas@gmail.com>
2017-07-19 10:13:06 +0200

    Get method is now using special coroutines

---
+++ Oddych/Assets/Src/TestEntryPoint.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace Oddych{
+	/// <summary>
+	/// Application entry point used for testing
+	/// </summary>
+	public class TestEntryPoint : MonoBehaviour {
+		
+		/************************
+		 * Implementation
+		 * *********************/
+		// Use this for initialization
+		void Start () {
+			//Test SpecialCoroutines
+			//Test1 test1 = gameObject.AddComponent<Test1> ();
+			//test1.Start ();
+			//Test2 test2 = gameObject.AddComponent<Test2> ();
+			//test2.Start ();
+			//Test3 test3 = gameObject.AddComponent<Test3> ();
+			//test3.Start ();
+
+			const String _URL = "http://jsonplaceholder.typicode.com/comments";
+			for (int i = 0; i < 10; i++) {
+				Debug.Log (DateTime.Now.ToString("hh:mm:ss.fff") +  " BEFORE getting data");
+				GetMethod getMethod = gameObject.AddComponent<GetMethod> ();
+				String data = getMethod.GetData (_URL);
+				//Debug.Log (data);
+				Debug.Log(DateTime.Now.ToString("hh:mm:ss.fff") + " AFTER getting data");
+			}
+		}
+
+		// Update is called once per frame
+		void Update () {
+		}
+
+		void SendPostRequest(){
+			
+			PostMethod test = gameObject.AddComponent<PostMethod>();
+			const String _URL = "http://jsonplaceholder.typicode.com/posts";
+			const String _WEBDATA = 
+				//"{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"some fancy title by Ondrej Mikulas\"}";
+				"{\n  \"userId\": 1,\n \"title\": \"some fancy title by Ondrej Mikulas\"}";
+			test.Start(_URL, _WEBDATA);
+			test.Start(_URL, _WEBDATA);
+			test.Start(_URL, _WEBDATA);
+			test.Start(_URL, _WEBDATA);
+			Debug.Log ("som tu");
+		}
+
+		//void SendDeleteRequest(){
+		//	GetMethod test = gameObject.AddComponent<GetMethod>();
+		//	const String _URL = "http://jsonplaceholder.typicode.com/posts";
+		//	test.Start(_URL);
+		//	Debug.Log ("som tu");
+		//}
+	} //class
+} //namespace
--- Oddych/Assets/Src/WebRequests/GetMethod.cs
+++ Oddych/Assets/Src/WebRequests/GetMethod.cs
@@ -11,68 +11,36 @@ namespace Oddych
 	/// </summary>
 	public class GetMethod : MonoBehaviour
 	{
-		/*******************
-		 * Initialization
-		 * ****************/
-		public GetMethod ()
-		{
-		}
-
 		/********************
 		 * Fields
 		 * *****************/
-
 		public UnityWebRequest Result;
 
 		/********************
 		 * Interface
 		 * *****************/
-
-		public void Start(){
+		public String GetData(String url){
+			StartCoroutine (GetSpecialCoroutine (url));
+			while (!Result.isDone) {
+			}
+			return Result.downloadHandler.text;
 		}
 
-		/// <summary>
-		/// Start getting data from specified url
-		/// </summary>
-		/// <param name="url">URL to server</param>
-		public void Start(String url){
-			StartCoroutine(GetText(url));
-		}
-
-		public UnityWebRequest StartNew(String url){
-			UnityWebRequest res = null;
-			StartCoroutine (GetText(url, result => res = result));
-			return res;
+		IEnumerator GetSpecialCoroutine(String url){
+			Coroutine<UnityWebRequest> routine = this.StartCoroutine<UnityWebRequest>(GetCoroutine(url)); //Start our new routine
+			yield return routine.coroutine; //Wait as we normally can
+			Result = routine.Value;
 		}
 
 		/*******************
 		 * Implementation
 		 * ****************/
-
-		IEnumerator GetText(String url, Action<UnityWebRequest> result){
-			if(String.IsNullOrEmpty(url)) { yield break; }
-			UnityWebRequest www = UnityWebRequest.Get (url);
-			www.Send ();
-			yield return null;
-			result(www);
-
-			print (www.downloadHandler.isDone);
-			if(string.IsNullOrEmpty(www.error) == false)
-			{
-				Debug.Log (url + " " + www.error);
-				result(null);
-				yield break;
-			}
-			//yield break;
-		}
-
-
 		/// <summary>
 		/// Gets the text from specified url
 		/// </summary>
 		/// <returns>Data from weberver, or error</returns>
 		/// <param name="url">URL to server</param>
-		IEnumerator GetText(String url){
+		IEnumerator GetCoroutine(String url){
 			UnityWebRequest www = UnityWebRequest.Get (url);
 			Result = www;
 			www.Send ();
@@ -80,39 +48,12 @@ namespace Oddych
 				yield return null;
 			}
 
-			Hashtable ht = ProcessJsonData (www.downloadHandler.text);
-			print ("sprava bola odoslana");
-			//yield return www.Send ();
-
 			if (www.isError) {
 				print (www.error);
 			} else {
 				// Show results as text
 				yield return www;
-
-
-				// Or retrieve results as binary data
-				byte[] results = www.downloadHandler.data;
 			}
-		}
-
-		private Hashtable ProcessJsonData(String jsonDataResults){
-			if (!jsonDataResults.Equals("")) {
-				// TODO use instead
-				//IList resultsIList = MiniJSON.Json.Deserialize (jsonDataResults) as IList;
-				int length = jsonDataResults.Length;
-				print ("return value length from JSON= " + length + " characters.");
-				print ("metoda GET vratila " + jsonDataResults);
-			}
-			return new Hashtable ();
-		}
-
-		private Hashtable _DictToHashtable(Dictionary<string, object> dict) {
-			var hashtable = new Hashtable();
-			foreach (var pair in dict) {
-				hashtable.Add(pair.Key, pair.Value);
-			}
-			return hashtable;
 		}
 	} //class
 } //namespace
